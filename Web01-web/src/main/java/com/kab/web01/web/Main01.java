d2ef755f1ad37f9ab73477b3bef021ea55a42946
Umadevi Raju <umadevibala2002@gmail.com>
2016-07-30 21:29:47 -0400

    Description changes
    name changed

--- Web01-web/src/main/java/com/kab/web01/web/Main01.java
+++ Web01-web/src/main/java/com/kab/web01/web/Main01.java
@@ -2,6 +2,6 @@ package com.kab.web01.web;
 
 public class Main01 {
 	public static void main(String[] args) {
-		System.out.println("Hello Uma...!");
+		System.out.println("Hello Uma Bala...!");
 	}
 }
